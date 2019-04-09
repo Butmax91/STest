4c73fbb1e185d69f22b3b6c3398857994213d0bb
Butmax91 <butmax1991@gmail.com>
2019-04-10 00:03:48 +0300

    add some

--- main.js
+++ main.js
@@ -17,17 +17,18 @@ function drowTable(tasks = checkLocalStorage()){
             let tableRow = document.createElement('tr');
             tableRow.innerHTML =
                 `
-                <td class="name cor">${tasks[i].taskName}</td>
-                <td class="status">
-                    <select>
-                        <option >Очікується</option>
-                        <option >В роботі</option>
-                        <option >Завершено</option>
+                <td class="name " data-id="${tasks[i].taskId}" ">${tasks[i].taskName}</td>
+                <td   >
+                    <select class="status" data-id="${tasks[i].taskId}">
+                        <option ${+tasks[i].taskStatus === 0 ? 'selected' : ''}>Очікується</option>
+                        <option ${+tasks[i].taskStatus === 1 ? 'selected' : ''}>В роботі</option>
+                        <option ${+tasks[i].taskStatus === 2 ? 'selected' : ''}>Завершено</option>
                     </select>
                 </td>
-                <td class="task cor">${tasks[i].taskDesc}</td>
-                <td class="date cor">${tasks[i].taskDate}</td>
-        `;
+                <td class="task cor" data-id="${tasks[i].taskId}" data-canBeChanged="${tasks[i].canBeChanged}" >${tasks[i].taskDesc}</td>
+                <td class="date cor" data-id="${tasks[i].taskId}">${tasks[i].taskDate}</td>
+                `;
+            tableRow.className = 'changed';
             tableBody.appendChild(tableRow);
         }
     }else{
@@ -79,11 +80,26 @@ function addTask(){
 
     addButton.addEventListener('click', (e)=>{
         e.preventDefault();
+        function getId(){
+            const tasks = checkLocalStorage();
+            const ids = [];
+            let id = Math.floor(Math.random()*1000);
+            console.log(id);
+            for (let i = 0; i <tasks.length ; i++) {
+               if (id === +tasks[i].taskId){
+                   return getId()
+               }
+            }
+            return id;
+        }
         task = {
             taskName : taskName.value.trim(),
-            taskStatus : 'Очікується',
+            taskStatus : 0,
             taskDesc : taskDesc.value.trim(),
             taskDate : taskDate.value.trim(),
+            taskId : getId(),
+            isChanged: false,
+            canBeChanged: true,
         };
         taskName.value = taskDesc.value = taskDate.value = '';
         addButton.disabled = true;
@@ -120,20 +136,89 @@ sortBy();
 
 function correction(){
     const table = document.querySelector('.taskList');
+    const data = checkLocalStorage();
     table.addEventListener('dblclick',(e)=>{
+        const data = checkLocalStorage();
         for (let i = 0; i < e.target.classList.length ; i++) {
-            if (e.target.classList[i] === 'cor'){
-                const style = getComputedStyle(e.target);
-                console.log(style);
-                let modal = document.createElement('div');
-                let correctionInput = document.createElement('input');
-                correctionInput.style.cssText = JSON.stringify(style);
+           console.log((e.target.classList[i] === 'cor') && !!e.target.dataset.canbechanged);
+           console.log(!!e.target.dataset.canbechanged);
+            if ((e.target.classList[i] === 'cor') && !!e.target.dataset.canbechanged ){
+                console.log(!e.target.dataset.canbechanged);
+                const modal = document.createElement('div');
+                const correctionInput = document.createElement('input');
+                correctionInput.value = e.target.innerText;
+                const addButton = document.createElement('button');
+                addButton.innerText = 'Change';
+                correctionInput.style.cssText =
+                    `
+                    position:absolute;
+                    top:${e.target.getBoundingClientRect().y+'px'};
+                    left:${e.target.getBoundingClientRect().x+'px'};
+                    width: ${e.target.getBoundingClientRect().width+'px'};
+                    height: ${e.target.getBoundingClientRect().height+'px'};
+                    padding:${getComputedStyle(e.target).padding};
+                    
+                    `;
+                addButton.style.cssText =
+                    `
+                    position:absolute;
+                    top:${e.target.getBoundingClientRect().y+e.target.getBoundingClientRect().height+10+'px'};
+                    left:${e.target.getBoundingClientRect().x +e.target.getBoundingClientRect().width - 
+                    60 +'px'};
+                    width:60px;
+                    padding:5px ;
+                    cursor:pointer;
+                    `;
                 modal.className = 'modal';
+                console.log(e.target.getBoundingClientRect());
                 modal.appendChild(correctionInput);
-
-                document.body.appendChild(modal)
+                modal.appendChild(addButton);
+                document.body.appendChild(modal);
+                modal.addEventListener('click',(event)=>{
+                        if(event.target === addButton){
+                        for (let j = 0; j < data.length  ; j++) {
+                            console.log(data[j].taskId);
+                            console.log(e.target);
+                            if (data[j].taskId === +e.target.dataset.id){
+                                data[j].taskDesc = correctionInput.value;
+                                data[j].isChanged = true;
+                                break;
+                            }
+                        }
+                        console.log(data);
+                        localStorage.tasks = JSON.stringify(data);
+                        drowTable();
+                        document.body.removeChild(modal);
+                    }
+                })
             }
         }
+    });
+    const select = document.querySelectorAll('select');
+    table.addEventListener('change',(e)=>{
+        const data = checkLocalStorage();
+        if(e.target.className === 'status'){
+            for (let i = 0; i <data.length ; i++) {
+                if (+data[i].taskId === +e.target.dataset.id){
+
+                    console.log(1)
+                    if(e.target.value === 'Завершено'){
+                            data[i].canBeChanged = false;
+                            data[i].taskStatus=2;
+                        }
+                    else if(e.target.value === 'В роботі') {
+                            data[i].canBeChanged = true;
+                            data[i].taskStatus = 1;
+
+                    }else{
+                        data[i].canBeChanged = true;
+                        data[i].taskStatus = 0;
+                    }
+                }
+            }
+        }
+        localStorage.tasks = JSON.stringify(data);
+        drowTable();
     })
 }
 correction();
